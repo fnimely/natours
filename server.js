c5d76300857ad4bfa68fc18ef59eafc6cc44dbdb
facsimile419 <dohfacsimile@gmail.com>
2022-05-04 06:29:28 -0700

    MongoDB Connection Configured connection and added a schema and model

--- server.js
+++ server.js
@@ -1,11 +1,47 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const app = require("./app");
 
 // read variables from config file and save them in node env variables
 dotenv.config({ path: "./config.env" });
 
-const app = require("./app");
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+console.log("The DB variable: ", DB);
+
+// options to deal deprecation warnings
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("DB connection successful"));
 
 // console.log(process.env);
+
+// creating a shcema - basic
+const toursSchema = new mongoose.Schema({
+  name: {
+    // schema type options
+    type: String,
+    required: [true, "A tour must have a name"],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price"], // validator used to validate data
+  },
+});
+
+// creating a model out of the schema - model is used to create a document
+const Tour = mongoose.model("Tour", toursSchema);
 
 const port = process.env.port || 3000;
 // start a server
