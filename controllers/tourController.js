7c8149644fcae5d1dcc81047bcc2765fde64d82b
facsimile419 <dohfacsimile@gmail.com>
2022-05-16 09:25:05 -0700

    Added fields limitation in queries and schema.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -42,6 +42,15 @@ exports.getAllTours = async (req, res) => {
       query = query.sort("-createdAt");
     }
 
+    // limiting fields / projecting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      // exculde __v field used by mongo
+      query = query.select("-__v");
+    }
+
     // { difficulty: 'easy', duration: { $gte 5 } }
 
     // creating query with mongoose
--- models/tourModels.js
+++ models/tourModels.js
@@ -51,6 +51,7 @@ const toursSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: Date.now(),
+    select: false, // hide field from query
   },
   startDates: [Date],
 });
