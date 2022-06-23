cfb322fb4a65f2dbf640248d8aa16f95db78e350
facsimile419 <dohfacsimile@gmail.com>
2022-06-22 19:34:38 -0700

    Error handling middleware

--- app.js
+++ app.js
@@ -34,9 +34,29 @@ app.use("/api/v1/users", userRouter);
 
 // unhandled routes
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    status: "fail",
-    message: `Can't find ${req.originalUrl} on this server.`,
+  // res.status(404).json({
+  //   status: "fail",
+  //   message: `Can't find ${req.originalUrl} on this server.`,
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server.`);
+  err.status = "fail";
+  err.statusCode = 404;
+
+  // a next function with a parameter is assumed to be an err, all
+  // other middleware is skipped.
+  next(err);
+});
+
+// error handling middleware
+app.use((err, req, res, next) => {
+  // assign the statusCode if it exist, else 500
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
