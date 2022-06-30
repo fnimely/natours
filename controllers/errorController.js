9e466c50057fc9342cedc195b8cede5f2d21875a
facsimile419 <dohfacsimile@gmail.com>
2022-06-30 08:19:39 -0700

    Handling invalid database IDs.

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,11 @@
+const AppError = require("./../utils/appError");
+
+// operational error - request on invalid url
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -38,6 +46,11 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === "development") {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === "production") {
-    sendErrorProd(err, res);
+    let error = { ...err };
+
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+
+    sendErrorProd(error, res);
   }
 };
