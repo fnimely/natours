587a1e384a295f3897cf36e94885d15bdbd2259a
facsimile419 <dohfacsimile@gmail.com>
2022-07-09 07:21:21 -0700

    Feature added to updated logged in user data.

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,17 @@
 // this files contains route handler middleware for all USER resources
 const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../utils/appError");
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) {
+      newObj[el] = obj[el];
+    }
+  });
+  return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -16,6 +27,34 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+// update the currently authenticated user
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // create an error if the user tries to update the password
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        "This route is not for password updates. Please use /updateMyPassword",
+        400
+      )
+    );
+  }
+
+  // update the user's doc
+  const filteredBody = filterObj(req.body, "name", "email"); // filter unwanted fields
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      user: updatedUser,
+    },
+  });
+});
+
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: "error",
