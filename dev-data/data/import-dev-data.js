29ad388e8fbc05f35b588e052f59addb24e49106
facsimile419 <dohfacsimile@gmail.com>
2022-07-14 00:00:50 -0700

    Users and Reviews data import.

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -5,6 +5,8 @@ const fs = require("fs");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const Tour = require("./../../models/tourModels");
+const User = require("./../../models/userModel");
+const Review = require("./../../models/reviewModel");
 
 // read variables from config file and save them in node env variables
 dotenv.config({ path: "./config.env" });
@@ -25,11 +27,17 @@ mongoose
 
 //   read json file
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, "utf-8"));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, "utf-8"));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, "utf-8")
+);
 
 // import data into db
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log("Data successfully created.");
     //   end running script
     process.exit();
@@ -42,6 +50,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log("Data successfully deleted.");
     //   end running script
     process.exit();
