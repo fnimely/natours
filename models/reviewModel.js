d9d1c3b0819af5a4af593bbe06b45867e275d355
facsimile419 <dohfacsimile@gmail.com>
2022-07-14 12:00:11 -0700

    Average Rating Calculated avg rating on a new review and updated new rating on tour.

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Tour = require("./tourModels");
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -38,5 +39,28 @@ reviewSchema.pre(/^find/, function (next) {
   next();
 });
 
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: { _id: "$tour" },
+      nRatings: { $sum: 1 },
+      avgRating: { $avg: "$rating" },
+    },
+  ]);
+  console.log(stats);
+
+  Tour.findByIdAndUpdate(tourId, {
+    ratingsQuantity: stats[0].nRatings,
+    ratingsAverage: stats[0].avgRating,
+  });
+};
+
+reviewSchema.post("save", function () {
+  // this points to current review
+  this.constructor.calcAverageRatings(this.tour);
+});
 const Review = mongoose.model("Review", reviewSchema);
 module.exports = Review;
