943d12b7212de4a095bb6ecd94c6a3fa05ada19d
facsimile419 <dohfacsimile@gmail.com>
2022-07-15 07:55:05 -0700

    Feature added to prevent duplicate reviews.

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -33,6 +33,9 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+// each combination of tour and user must be unique
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function (next) {
   // this.populate("user", "name photo").populate("tour", "name");
   this.populate("user", "name photo");
