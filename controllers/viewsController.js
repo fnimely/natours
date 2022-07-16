a3358933fe23c97bdf5691136eba9c2a92fd48ad
facsimile419 <dohfacsimile@gmail.com>
2022-07-16 07:35:06 -0700

    Pug overview added + templates removed.

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,19 @@
-exports.getOverview = (req, res) => {
+const Tour = require("../models/tourModels");
+const catchAsync = require("../utils/catchAsync");
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // get tour data
+  const tours = await Tour.find();
+
+  // build template
+
+  // render template using tour data
+
   res.status(200).render("overview", {
     title: "All tours",
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render("base", {
