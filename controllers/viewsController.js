6e435310036222fc34983e858a061b8ad09235d5
facsimile419 <dohfacsimile@gmail.com>
2022-07-18 19:33:48 -0700

    Log in page added.

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -2,6 +2,7 @@ const Tour = require("../models/tourModels");
 const User = require("../models/userModel");
 const Review = require("../models/reviewModel");
 const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
 
 exports.getOverview = catchAsync(async (req, res, next) => {
   // get tour data
@@ -17,7 +18,7 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findOne({ slug: req.params.slug }).populate(
     "reviews",
     "review rating user"
@@ -26,5 +27,11 @@ exports.getTour = catchAsync(async (req, res) => {
   res.status(200).render("tour", {
     title: `${tour.name} Tour`,
     tour,
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  res.status(200).render("login", {
+    title: "Log into your account",
   });
 });
