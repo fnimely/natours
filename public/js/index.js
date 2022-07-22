4a2d1ac0c8f4ffce282675f354c938f67e9e1e80
facsimile419 <dohfacsimile@gmail.com>
2022-07-22 09:16:04 -0700

    Updating user data - password

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,12 @@
 import "@babel/polyfill";
 import { displayMap } from "./mapbox";
 import { login, logout } from "./login";
-import { updateData } from "./updateSettings";
+import { updateSettings } from "./updateSettings";
 
 const mapBox = document.getElementById("map");
 const loginForm = document.querySelector(".form--login");
 const updateForm = document.querySelector(".form-user-data");
+const userPasswordForm = document.querySelector(".form-user-password");
 const logoutBtn = document.querySelector(".nav__el--logout");
 
 if (mapBox) {
@@ -28,7 +29,28 @@ if (updateForm) {
 
     const email = document.getElementById("email").value;
     const name = document.getElementById("name").value;
-    updateData(name, email);
+    updateSettings({ name, email }, "data");
+  });
+}
+
+if (userPasswordForm) {
+  userPasswordForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    document.querySelector(".btn--save-password").textContent = "updating...";
+
+    const passwordCurrent = document.getElementById("password-current").value;
+    const password = document.getElementById("password").value;
+    const passwordConfirm = document.getElementById("password-confirm").value;
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      "password"
+    );
+
+    document.querySelector(".btn--save-password").textContent = "Save";
+    document.getElementById("password-current").value = "";
+    document.getElementById("password").value = "";
+    document.getElementById("password-confirm").value = "";
   });
 }
 
--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -1,19 +1,22 @@
 import axios from "axios";
 import { showAlert } from "./alert";
 
-export const updateData = async (name, email) => {
+// type is either password or data
+export const updateSettings = async (data, type) => {
   try {
+    const url =
+      type === "password"
+        ? "http://127.0.0.1:3000/api/v1/users/updateMyPassword"
+        : "http://127.0.0.1:3000/api/v1/users/updateMe";
+
     const res = await axios({
       method: "PATCH",
-      url: "http://127.0.0.1:3000/api/v1/users/updateMe",
-      data: {
-        name,
-        email,
-      },
+      url,
+      data,
     });
 
     if (res.data.status === "success") {
-      showAlert("success", "Data updated successful");
+      showAlert("success", `${type.toUpperCase()} updated successful`);
       window.setTimeout(() => {
         location.reload();
       }, 1000);
