6e71ca716e667eb3abd612203eecd59a2ea81b98
facsimile419 <dohfacsimile@gmail.com>
2022-07-29 10:22:54 -0700

    URL change

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -7,9 +7,7 @@ export const bookTour = async (tourId) => {
   );
   try {
     // get the session from server
-    const session = await axios(
-      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
-    );
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
 
     // create checkout form + charge credit card
     await stripe.redirectToCheckout({
