e30e959de7a4d7f067a2fb7c644b68a2837abab8
facsimile419 <dohfacsimile@gmail.com>
2022-07-29 07:36:16 -0700

    Bookings API - CRUD

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -3,6 +3,7 @@ const bookingController = require("./../controllers/bookingController");
 const authController = require("./../controllers/authController");
 
 const router = express.Router();
+router.use(authController.protect);
 
 router.get(
   "/checkout-session/:tourId",
@@ -10,4 +11,17 @@ router.get(
   bookingController.getCheckoutSession
 );
 
+router.use(authController.restrictTo("admin", "lead-guide"));
+
+// creating, getting, reading, and updating bookings
+router
+  .route("/")
+  .get(bookingController.getAllBookings)
+  .post(bookingController.createBooking);
+
+router
+  .route("/:id")
+  .get(bookingController.getBooking)
+  .patch(bookingController.updateBooking)
+  .delete(bookingController.deleteBooking);
 module.exports = router;
