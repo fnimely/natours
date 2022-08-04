ad1ba47fe9ec957c8f7a447eb8f9e726c6377899
facsimile419 <dohfacsimile@gmail.com>
2022-08-04 09:49:14 -0700

    COORS configs + engine version restriction.

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const xss = require("xss-clean");
 const hpp = require("hpp");
 const cookieParser = require("cookie-parser");
 const compression = require("compression");
+const cors = require("cors");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -27,6 +28,9 @@ app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
 // global MIDDLEWARES
+
+app.use(cors());
+app.options("*", cors()); // non-simple request
 
 // express middlware used to serve static files
 app.use(express.static(path.join(__dirname, "public")));
